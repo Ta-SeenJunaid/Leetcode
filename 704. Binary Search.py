c276a41761bd1932cc821a7d98499cac50263f82
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-05-26 17:17:46 +0200

    704. Binary Search.py

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -1,20 +1,40 @@
 from typing import List
 
 
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         return self.recursive_binary_search(nums, target, 0, len(nums) - 1)
+#
+#     def recursive_binary_search(self, nums: List[int], target: int, left: int, right: int) -> int:
+#
+#         if left > right:
+#             return -1
+#
+#         mid = (left + right)//2
+#
+#         if target < nums[mid]:
+#             return self.recursive_binary_search(nums, target, left, mid - 1)
+#         if target > nums[mid]:
+#             return self.recursive_binary_search(nums, target, mid + 1, right)
+#
+#         return mid
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return self.recursive_binary_search(nums, target, 0, len(nums) - 1)
+        left = 0
+        right = len(nums)-1
+        while left<=right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                right = mid -1
+            elif target > nums[mid] :
+                left = mid + 1
+        return -1
 
-    def recursive_binary_search(self, nums: List[int], target: int, left: int, right: int) -> int:
 
-        if left > right:
-            return -1
-
-        mid = (left + right)//2
-
-        if target < nums[mid]:
-            return self.recursive_binary_search(nums, target, left, mid -1)
-        if target > nums[mid]:
-            return self.recursive_binary_search(nums, target, mid +1, right)
-
-        return mid
+solution = Solution()
+print(solution.search(nums = [-1,0,3,5,9,12], target = 9))
+print(solution.search(nums = [-1,0,3,5,9,12], target = 2))
