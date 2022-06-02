62594482a0a0573336184707d874340cf9b3059f
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-06-02 09:04:06 +0200

    867. Transpose Matrix.py

---
+++ 867. Transpose Matrix.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        return list(map(list, zip(matrix)))
+
+
+solution = Solution()
+print(solution.transpose(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
+print(solution.transpose([[1,2,3],[4,5,6]]))
