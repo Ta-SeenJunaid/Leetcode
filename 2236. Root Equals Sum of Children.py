c1a32073ce68414e640923922449094ac46604c6
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-06-13 07:54:19 +0200

    2236. Root Equals Sum of Children.py

---
+++ 2236. Root Equals Sum of Children.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val == root.left.val + root.right.val:
+            return True
+        else:
+            return False
