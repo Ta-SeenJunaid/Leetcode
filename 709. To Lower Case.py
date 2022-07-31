c2be12a2ca2dae04efd50b62ae24a1b5d5aa948e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-07-31 09:19:09 +0200

    709. To Lower Case.py

---
+++ 709. To Lower Case.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+
+solution = Solution()
+print(solution.toLowerCase(s = "Hello"))
+print(solution.toLowerCase(s = "here"))
+print(solution.toLowerCase(s = "LOVELY"))
