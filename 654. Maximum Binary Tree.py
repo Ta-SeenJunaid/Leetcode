b62912cfa5e3a807416b65212cd66f86205aff0a
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-08-10 22:33:54 +0200

    654. Maximum Binary Tree.py

---
+++ 654. Maximum Binary Tree.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return
+        max_nums = max(nums)
+        max_index = nums.index(max_nums)
+        root = TreeNode(max_nums)
+        root.left = self.constructMaximumBinaryTree(nums[:max_index])
+        root.right = self.constructMaximumBinaryTree(nums[max_index+1:])
+        return root
+
