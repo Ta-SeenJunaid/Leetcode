79f7ceb44a1065ca64c7811d9d972f30592aa957
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-08-15 09:47:49 +0200

    17. Letter Combinations of a Phone Number.py

---
+++ 17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        pass
+
+
+solution = Solution()
+print(solution.letterCombinations())
+print(solution.letterCombinations())
+print(solution.letterCombinations())
