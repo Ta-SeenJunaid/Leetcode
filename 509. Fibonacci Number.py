56a576fecc5391287e482866b0dcf677d7b9ea1f
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-09-20 11:44:26 +0200

    509. Fibonacci Number.py with DP

--- 509. Fibonacci Number.py
+++ 509. Fibonacci Number.py
@@ -1,11 +1,30 @@
+# class Solution(object):
+#     def fib(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         if n == 0:
+#             return 0
+#         if n == 1:
+#             return 1
+#         return self.fib(n-1) + self.fib(n-2)
+
+# Dynamic programming
 class Solution(object):
+    def __init__(self):
+        self.memory = {}
     def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
         if n == 0:
             return 0
         if n == 1:
             return 1
-        return self.fib(n-1) + self.fib(n-2)
+        if n in self.memory:
+            return self.memory[n]
+        self.memory[n] = self.fib(n-1) + self.fib(n-2)
+        return self.memory[n]
+
+
+solution = Solution()
+print(solution.fib(30))
+print(solution.fib(40))
