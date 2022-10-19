351466125ca6a2a94cfd9e82b7e14390b9adaa91
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-10-19 07:40:00 +0200

    692. Top K Frequent Words.py

---
+++ 692. Top K Frequent Words.py
@@ -0,0 +1,15 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # words = sorted(words)
+        return [i[0] for i in collections.Counter(sorted(words)).most_common(k)]
+
+
+solution = Solution()
+print(solution.topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 2))
+print(solution.topKFrequent(words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4))
+
+
