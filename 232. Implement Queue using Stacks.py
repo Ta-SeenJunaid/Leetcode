ea72f91a52029cfe88d678b8ed7dd945ce4d3745
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-12-16 05:40:05 +0100

    232. Implement Queue using Stacks.py

---
+++ 232. Implement Queue using Stacks.py
@@ -0,0 +1,16 @@
+class MyQueue:
+
+    def __init__(self):
+        self.q = []
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop()
+
+    def peek(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return not len(self.q)
