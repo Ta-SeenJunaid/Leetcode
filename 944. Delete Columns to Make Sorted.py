687ddab005e562023ba1a8a738549d323a0190cf
Ta-SeenJunaid <taseen.junaid@gmail.com>
2023-01-03 03:59:01 +0100

    944. Delete Columns to Make Sorted.py

---
+++ 944. Delete Columns to Make Sorted.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        ans = 0
+        for j in range(len(strs[0])):
+            for i in range(len(strs)-1):
+                if strs[i][j] > strs[i+1][j]:
+                    ans += 1
+                    break
+        return ans
+
+solution = Solution()
+print(solution.minDeletionSize(strs = ["cba","daf","ghi"]))
+print(solution.minDeletionSize(strs = ["a","b"]))
+print(solution.minDeletionSize(strs = ["zyx","wvu","tsr"]))
