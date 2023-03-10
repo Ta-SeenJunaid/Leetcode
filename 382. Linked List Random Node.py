3552f173f90df7fdae9d8b9763b667d52de29d3b
Ta-SeenJunaid <taseen.junaid@gmail.com>
2023-03-10 10:46:59 +0100

    382. Linked List Random Node.py

---
+++ 382. Linked List Random Node.py
@@ -0,0 +1,24 @@
+import random
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.linked_list = []
+        while head:
+            self.linked_list.append(head.val)
+            head = head.next
+
+    def getRandom(self) -> int:
+        return random.choice(self.linked_list)
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
