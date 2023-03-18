e5433e2f196c9aa2c4bcae7d8d53f497dea3739d
Ta-SeenJunaid <taseen.junaid@gmail.com>
2023-03-18 05:53:46 +0100

    1472. Design Browser History.py

---
+++ 1472. Design Browser History.py
@@ -0,0 +1,29 @@
+# https://www.youtube.com/watch?v=i1G-kKnBu8k
+
+class ListNode:
+    def __init__(self, val, prev = None, next = None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.cur = ListNode(homepage)
+
+    def visit(self, url: str) -> None:
+        self.cur.next = ListNode(url, prev=self.cur)
+        self.cur = self.cur.next
+
+    def back(self, steps: int) -> str:
+        while self.cur.prev and steps > 0:
+            self.cur = self.cur.prev
+            steps -= 1
+        return self.cur.val
+
+    def forward(self, steps: int) -> str:
+        while self.cur.next and steps > 0:
+            self.cur = self.cur.next
+            steps -= 1
+        return self.cur.val
