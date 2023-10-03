fadf6772f88d6cc59a3e07046a1694d44d100033
Ta-Seen Junaid <taseen.junaid@gmail.com>
2023-10-03 07:49:05 +0200

    Update 1512. Number of Good Pairs.py

--- 1512. Number of Good Pairs.py
+++ 1512. Number of Good Pairs.py
@@ -1,17 +1,9 @@
-from typing import List
-
-
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count = 0
-        for i in range(0, len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    count += 1
-        return count
-
-
-solution = Solution()
-print(solution.numIdenticalPairs(nums = [1,2,3,1,1,3]))
-print(solution.numIdenticalPairs(nums = [1,1,1,1]))
-print(solution.numIdenticalPairs(nums = [1,2,3]))
+        counts = defaultdict(int)
+        ans = 0
+        for num in nums:
+            ans += counts[num]
+            counts[num] += 1
+        return ans
+        
