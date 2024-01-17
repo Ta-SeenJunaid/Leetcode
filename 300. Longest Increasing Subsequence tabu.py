bacc2b11163002daa13fbfb65f85081403934957
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-01-17 18:08:14 +0100

    Update 300. Longest Increasing Subsequence tabu.py

--- 300. Longest Increasing Subsequence tabu.py
+++ 300. Longest Increasing Subsequence tabu.py
@@ -1,9 +1,10 @@
+# https://www.youtube.com/watch?v=cjWnW0hdF1Y
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        tabu = [[0 for _ in range(len(nums) + 1)] for _ in range(len(nums) + 1)]
-        for idx in range(len(nums)-1, -1, -1):
-            for pre_idx in range(idx-1, -2, -1):
-                tabu[idx][pre_idx+1] = max((1 + tabu[idx + 1][idx + 1]) if (pre_idx == -1 or nums[idx] > nums[pre_idx]) else 0,\
-                 tabu[idx+1][pre_idx+1])
-        return tabu[0][-1+1]
-        
+        memo = [1 for _ in range(len(nums))]
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums), 1):
+                if nums[i] < nums[j]:
+                    memo[i] = max(memo[i], 1 + memo[j])
+        return max(memo)
