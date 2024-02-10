f6d82335d35bcaa1e344f84af0b2ec1d402a035a
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-02-10 20:00:51 +0100

    Create 1547. Minimum Cost to Cut a Stick memo.py

---
+++ 1547. Minimum Cost to Cut a Stick memo.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.memo = None
+        
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts.append(n)
+        cuts.append(0)
+        cuts.sort()
+        self.memo = [[-1 for _ in range(len(cuts))] for _ in range(len(cuts))]
+        return self.cutting(1, len(cuts)-2, cuts)
+
+    def cutting(self, i, j, cuts):
+        if i > j:
+            return 0
+        mini = float('inf')
+        if self.memo[i][j] != -1:
+            return self.memo[i][j]
+        for idx in range(i, j+1):
+            mini = min(mini, cuts[j+1] - cuts[i-1] + self.cutting(i, idx-1, cuts) + self.cutting(idx+1, j, cuts))
+        self.memo[i][j] = mini
+        return mini
