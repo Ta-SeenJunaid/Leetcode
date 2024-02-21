e2d5e0ed82395a905cdb4f964d1dab54fae8ede6
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-02-21 12:21:38 +0100

    Update 39. Combination Sum.py

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -1,38 +1,18 @@
-from typing import List
-
-
 class Solution:
     def __init__(self):
-        self.temp = []
+        self.ans = []
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.combination_sum_helper(candidates, target, [])
-        result_len = len(self.temp)
-        result = []
-        for i in range(result_len):
-            self.temp[i].sort()
-            if self.temp[i] not in result:
-                result.append(self.temp[i])
-        return result
+        candidates.sort()
+        self.recur(0, target, [], candidates)
+        return self.ans
 
-
-    def combination_sum_helper(self, candidates: List[int], target: int, current_candidates: List[int]):
-        if target == 0:
-            self.temp.append(current_candidates)
+    def recur(self, idx, cur_target, cur_list, candidates):
+        if cur_target == 0:
+            if cur_list not in self.ans:
+                self.ans.append(cur_list)
+                return
+        if cur_target < 0 or idx >= len(candidates):
             return
-        if target < 0:
-            return
-
-        for candidate in candidates:
-            reminder = target - candidate
-            temp_current_candidates = []
-            temp_current_candidates = current_candidates + [candidate]
-            if reminder >= 0:
-                # print(current_candidates)
-                self.combination_sum_helper(candidates, reminder, temp_current_candidates)
-
-
-solution = Solution()
-print(solution.combinationSum(candidates = [2,3,6,7], target = 7))
-# print(solution.combinationSum(candidates = [2,3,5], target = 8))
-# print(solution.combinationSum(candidates = [2], target = 1))
+        self.recur(idx+1, cur_target, cur_list, candidates)
+        self.recur(idx, cur_target-candidates[idx], cur_list + [candidates[idx]], candidates)
