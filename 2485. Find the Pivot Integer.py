3c733f60b41193e855e876190788ec455cec3d8a
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-03-13 12:49:37 +0100

    Update 2485. Find the Pivot Integer.py

--- 2485. Find the Pivot Integer.py
+++ 2485. Find the Pivot Integer.py
@@ -1,14 +1,9 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         p_sum1 = 0
-        for i in range(n+1):
-            p_sum1 += i
-            p_sum2 = 0
-            j = i
-            while j <=n:
-                p_sum2 += j
-                j += 1
-            if p_sum1 == p_sum2:
+        for i in range(1, n+1):
+            sum_left = sum(range(1, i+1))
+            sum_right = sum(range(i, n+1))
+            if sum_left == sum_right:
                 return i
-            i += 1
         return -1
