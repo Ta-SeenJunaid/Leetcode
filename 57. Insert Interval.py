a6d09b43dc96864be09b28315be7c675e09856c5
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-03-17 13:03:33 +0100

    Update 57. Insert Interval.py

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -1,23 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ans = []
-        if intervals == ans:
-            return [newInterval]
-        if newInterval[1] < intervals[0][0]:
-            return [newInterval] + intervals
-        for i in range(len(intervals)):
-            if intervals[i][1] >= newInterval[0]:
-                j = i
-                while j < len(intervals) and newInterval[1] >= intervals[j][0]:
-                    j += 1
-                ans.append([intervals[i][0] if intervals[i][0] < newInterval[0] else newInterval[0], 
-                intervals[j-1][1] if intervals[j-1][1] > newInterval[1] else newInterval[1]])
-                while j < len(intervals):
-                    ans.append(intervals[j])
-                    j += 1
-                break
+        i = 0
+        itv_len = len(intervals)
+
+        while i < itv_len and intervals[i][1] < newInterval[0]:
             ans.append(intervals[i])
-            if i == len(intervals) - 1:
-                ans.append(newInterval)
+            i += 1
+
+        while i < itv_len and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        ans.append(newInterval)
+
+        while i < itv_len:
+            ans.append(intervals[i])
+            i += 1
+        
         return ans
+
             
