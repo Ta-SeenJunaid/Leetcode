ccf1e24d09119d34150eaf20cc90ce4806818836
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-07-04 17:13:37 +0200

    Update 2181. Merge Nodes in Between Zeros.py

--- 2181. Merge Nodes in Between Zeros.py
+++ 2181. Merge Nodes in Between Zeros.py
@@ -3,22 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        modified_list = None
-        while head:
-            if head.val != 0:
-                count = 0
-                while head.val != 0:
-                    count += head.val
-                    head = head.next
-                if modified_list:
-                    temp = ListNode(val=count)
-                    modified_list.next = temp
-                    modified_list = modified_list.next
-                else:
-                    modified_list = ListNode(val=count)
-                    ans = modified_list
-            head = head.next
-        return ans
+        modify = head.next
+        next_sum = modify
+        while next_sum:
+            sum = 0
+            while next_sum.val != 0:
+                sum += next_sum.val
+                next_sum = next_sum.next
+            modify.val = sum
+            next_sum = next_sum.next
+            modify.next = next_sum
+            modify = modify.next
+        return head.next
         
