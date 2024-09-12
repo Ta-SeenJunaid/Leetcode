1ed2a0424e2e9bc223db1b484831f607e6faf9a9
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-09-12 07:49:03 +0200

    Create 339. Nested List Weight Sum.py

---
+++ 339. Nested List Weight Sum.py
@@ -0,0 +1,57 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def __init__(self):
+        self.total = 0
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        self.dfs_helper(nestedList, 1)
+        return self.total
+
+    def dfs_helper(self, input, depth):
+        for i in input:
+            if i.isInteger():
+                self.total += i.getInteger() * depth
+            else:
+                self.dfs_helper(i.getList(), depth + 1)
+        
