66ae128c085f98c0bc3afe6f1ceeac5b2acbfd74
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-09-19 16:44:29 +0200

    Update 547. Number of Provinces.py

--- 547. Number of Provinces.py
+++ 547. Number of Provinces.py
@@ -1,34 +1,23 @@
-from collections import defaultdict
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self.adj_list = defaultdict(list)
-        self.visited = set()
-        self.result = 0
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        adj_list = defaultdict(list)
         for i in range(len(isConnected)):
-            for j in range(len(isConnected)):
-                if i !=j and isConnected[i][j] == 1:
-                    self.adj_list[i].append(j)
+            for j in range(len(isConnected[0])):
+                if i!=j and isConnected[i][j] == 1:
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
+        visited = set()
+        stack = []
+        pro = 0
         for i in range(len(isConnected)):
-            if i not in self.visited:
-                self.result += 1
-                self.dfs(i)
-        return self.result
-
-    def dfs(self, node):
-        self.visited.add(node)
-        for i in self.adj_list[node]:
-            if i not in self.visited:
-                self.dfs(i)
-
-
-
-
-solution = Solution()
-print(solution.findCircleNum(isConnected = [[1,1,0],[1,1,0],[0,0,1]]))
-
-solution = Solution()
-print(solution.findCircleNum(isConnected = [[1,0,0],[0,1,0],[0,0,1]]))
+            if i not in visited:
+                visited.add(i)
+                stack.append(i)
+                pro += 1
+                while(stack):
+                    c_node = stack.pop(0)
+                    for nei in adj_list[c_node]:
+                        if nei not in visited:
+                            visited.add(nei)
+                            stack.append(nei)
+        return pro
