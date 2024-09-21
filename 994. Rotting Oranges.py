fad133b8cfccb6311008a51dfa5ef91653db627d
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-09-21 14:59:15 +0200

    Update 994. Rotting Oranges.py

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -1,71 +1,44 @@
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self.fresh_orange = 0
-        self.queue = []
-        self.visited = set()
-        self.count = 0
-        self.r_len = 0
-        self.c_len = 0
-        pass
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        self.r_len = len(grid)
-        self.c_len = len(grid[0])
-        for i in range(self.r_len):
-            for j in range(self.c_len):
-                if grid[i][j]==0:
+        visited = set()
+        queue = []
+        cc = 0
+        t = -1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 0:
                     continue
-                elif grid[i][j] == 2:
-                    self.queue.append([i,j])
-                else:
-                    self.fresh_orange += 1
-        if self.fresh_orange == 0:
+                elif grid[r][c] == 2:
+                    queue.append((r,c))
+                elif grid[r][c] == 1:
+                    cc += 1
+        if cc == 0:
             return 0
-        if len(self.queue) == 0:
+        if len(queue) == 0:
             return -1
-        while self.queue:
-            queue_len = len(self.queue)
-            for _ in range(queue_len):
-                temp = self.queue.pop(0)
-                r, c = temp[0], temp[1]
-                # if r < 0 or r >= self.r_len or c < 0 or c >= self.c_len or (r, c) in self.visited or (r, c)!=1:
-                #     continue
-                if (r, c) not in self.visited:
-                    self.visited.add((r,c))
-                if r+1 < self.r_len and grid[r+1][c] == 1 and (r+1, c) not in self.visited:
-                    # print(r+1, c)
-                    self.queue.append([r+1, c])
-                    self.fresh_orange -= 1
-                    self.visited.add((r+1, c))
-                if r-1 >= 0 and grid[r-1][c] == 1 and (r-1, c) not in self.visited:
-                    # print(r-1, c)
-                    self.queue.append([r-1, c])
-                    self.fresh_orange -= 1
-                    self.visited.add((r-1, c))
-                if c+1 < self.c_len and grid[r][c+1] == 1 and (r, c+1) not in self.visited:
-                    # print(r, c+1)
-                    self.queue.append([r, c+1])
-                    self.fresh_orange -= 1
-                    self.visited.add((r, c+1))
-                if c-1 >= 0 and grid[r][c-1] == 1 and (r, c-1) not in self.visited:
-                    # print(r, c-1)
-                    self.queue.append([r, c-1])
-                    self.fresh_orange -= 1
-                    self.visited.add((r, c-1))
-            self.count += 1
-        if self.fresh_orange:
+        while queue:
+            for _ in range(len(queue)):
+                r, c = queue.pop(0)
+                if r+1 < len(grid) and (r+1, c) not in visited and grid[r+1][c] == 1:
+                    visited.add((r+1, c))
+                    cc -= 1
+                    queue.append((r+1, c))
+                if r-1 >= 0 and (r-1, c) not in visited and grid[r-1][c] == 1:
+                    visited.add((r-1, c))
+                    cc -= 1
+                    queue.append((r-1, c))
+                if c+1 < len(grid[0]) and (r, c+1) not in visited and grid[r][c+1] == 1:
+                    visited.add((r, c+1))
+                    cc -= 1
+                    queue.append((r, c+1))
+                if c-1 >= 0 and (r, c-1) not in visited and grid[r][c-1] == 1:
+                    visited.add((r, c-1))
+                    cc -= 1
+                    queue.append((r, c-1))
+            t += 1
+        if cc != 0:
             return -1
-        return self.count-1
+        return t
 
-
-
-solution = Solution()
-print(solution.orangesRotting(grid = [[2,1,1],[1,1,0],[0,1,1]]))
-
-solution = Solution()
-print(solution.orangesRotting(grid = [[2,1,1],[0,1,1],[1,0,1]]))
-
-solution = Solution()
-print(solution.orangesRotting(grid = [[0,2]]))
+                
+                
