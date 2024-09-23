f7f0f85e6ee3025e6c8d6118ae1e7129be6487b1
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-09-23 20:41:57 +0200

    Update 1020. Number of Enclaves.py

--- 1020. Number of Enclaves.py
+++ 1020. Number of Enclaves.py
@@ -1,47 +1,24 @@
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self.visited = set()
-        self.r_len = 0
-        self.c_len = 0
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        self.r_len = len(grid)
-        self.c_len = len(grid[0])
+        def dfs_helper(r, c):
+            if r < 0 or r == len(grid) or c < 0 or c == len(grid[0]):
+                return
+            elif grid[r][c] == 0:
+                return
+            elif (r, c) not in visited:
+                visited.add((r,c))
+                for rd, cd in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    dfs_helper(r+rd, c+cd)
+        visited = set()
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if (row == 0 or col == 0 or row == len(grid)-1 or col == len(grid[0])-1) and grid[row][col] == 1:
+                    dfs_helper(row, col)
         ans = 0
-        for j in range(self.c_len):
-            if grid[0][j] == 1:
-                self.dfs(0, j, grid)
-            if grid[self.r_len-1][j] == 1:
-                self.dfs(self.r_len-1, j, grid)
-        for i in range(1, self.r_len-1):
-            if grid[i][0] == 1:
-                self.dfs(i, 0, grid)
-            if grid[i][self.c_len-1] == 1:
-                self.dfs(i, self.c_len-1, grid)
-        for i in range(1, self.r_len-1):
-            for j in range(1, self.c_len-1):
-                if grid[i][j] == 1 and (i, j) not in self.visited:
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1 and (row, col) not in visited:
                     ans += 1
         return ans
 
-    def dfs(self, r, c, grid):
-        if r < 0 or r >= self.r_len or c < 0 or c >= self.c_len or (r, c) in self.visited or grid[r][c] == 0:
-            return
-        self.visited.add((r, c))
-        self.dfs(r+1, c, grid)
-        self.dfs(r-1, c, grid)
-        self.dfs(r, c+1, grid)
-        self.dfs(r, c-1, grid)
-        return
-
-
-solution = Solution()
-print(solution.numEnclaves(grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]))
-
-solution = Solution()
-print(solution.numEnclaves(grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]))
-
-solution = Solution()
-print(solution.numEnclaves(grid = [[0],[1],[1],[0],[0]]))
+        
