3bb3ccd3d72facc93538fb4ee170e5061cc99af5
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-09-23 20:12:27 +0200

    Update 130. Surrounded Regions.py

--- 130. Surrounded Regions.py
+++ 130. Surrounded Regions.py
@@ -1,50 +1,27 @@
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self.visited = set()
-        self.r_len = 0
-        self.c_len = 0
-
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        self.r_len = len(board)
-        self.c_len = len(board[0])
-        for j in range(self.c_len):
-            if board[0][j] == 'O':
-                self.dfs(0, j, board)
-            if board[self.r_len-1][j] == 'O':
-                self.dfs(self.r_len-1, j, board)
-        for i in range(1, self.r_len-1):
-            if board[i][0] == 'O':
-                self.dfs(i, 0, board)
-            if board[i][self.c_len-1] == 'O':
-                self.dfs(i, self.c_len-1, board)
-        for i in range(1, self.r_len-1):
-            for j in range(1, self.c_len-1):
-                if board[i][j] == 'O' and (i, j) not in self.visited:
-                    board[i][j] = 'X'
+        visited = set()
+        def dfs_helper(r, c, board):
+            if r < 0 or c < 0 or r == len(board) or c == len(board[0]):
+                return
+            elif board[r][c] == "X":
+                return
+            elif (r, c) not in visited:
+                visited.add((r,c))
+                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                for rd, cd in directions:
+                    dfs_helper(r+rd, c+cd, board)
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if (row == 0 or row == len(board)-1 or col == 0 or col ==len(board[0])-1 ) and board[row][col] == "O":
+                    dfs_helper(row, col, board) 
 
-    def dfs(self, r, c, board: List[List[str]]):
-        if r < 0 or r >= self.r_len or c < 0 or c >= self.c_len or (r, c) in self.visited or board[r][c] != 'O':
-            return
-        self.visited.add((r,c))
-        self.dfs(r+1, c, board)
-        self.dfs(r-1, c, board)
-        self.dfs(r, c+1, board)
-        self.dfs(r, c-1, board)
-        return
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == "O" and (row, col) not in visited:
+                    print(row, col)
+                    board[row][col] = "X"               
 
-
-solution = Solution()
-print(solution.solve(board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]))
-
-solution = Solution()
-print(solution.solve(board = [["X"]]))
-
-solution = Solution()
-print(solution.solve(board = [["O","X","O","O","X","X"],["O","X","X","X","O","X"],["X","O","O","X","O","O"],
-                              ["X","O","X","X","X","X"],["O","O","X","O","X","X"],["X","X","O","O","O","O"]]))
