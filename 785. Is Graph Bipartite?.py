46c02393a9bf3993eb7267ad5158a4eec4e640b9
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-10-05 05:36:45 +0200

    Update 785. Is Graph Bipartite?.py

--- 785. Is Graph Bipartite?.py
+++ 785. Is Graph Bipartite?.py
@@ -1,53 +1,18 @@
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self.visited = set()
-        self.queue = []
-        self.set_a = set()
-        self.set_b = set()
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        visited = [-1] * len(graph)
+        stack = []
         for i in range(len(graph)):
-            if i not in self.visited:
-                self.bfs(i, i%2, graph)
-        return False if self.set_a.intersection(self.set_b) else True
-
-    def bfs(self, root: int, set_g: int, graph: List[List[int]]):
-        self.visited.add(root)
-        if set_g == 0:
-            self.set_a.add(root)
-        else:
-            self.set_b.add(root)
-        self.queue.append(root)
-        while self.queue:
-            set_g = 1 - set_g
-            for _ in range(len(self.queue)):
-                current_root = self.queue.pop(0)
-                for i in graph[current_root]:
-                    if i not in self.visited:
-                        self.visited.add(i)
-                        self.queue.append(i)
-                    if set_g == 0:
-                        self.set_a.add(i)
-                    else:
-                        self.set_b.add(i)
-        return
-
-
-
-    # def dfs(self, root: int, graph: List[List[int]]):
-    #     if root in self.visited:
-    #         return
-    #     self.visited.add(root)
-    #     print(root)
-    #     for i in graph[root]:
-    #         if i not in self.visited:
-    #             self.dfs(i, graph)
-    #     return
-
-solution = Solution()
-print(solution.isBipartite(graph = [[1,2,3],[0,2],[0,1,3],[0,2]]))
-
-solution = Solution()
-print(solution.isBipartite(graph = [[1,3],[0,2],[1,3],[0,2]]))
+            if visited[i] == -1:
+                visited[i] = 0
+                stack = [i]
+                while stack:
+                    node = stack.pop()
+                    color = visited[node]
+                    for nei in graph[node]:
+                        if visited[nei] == -1:
+                            stack.append(nei)
+                            visited[nei] = 1-color
+                        elif visited[nei] == color:
+                            return False
+        return True
