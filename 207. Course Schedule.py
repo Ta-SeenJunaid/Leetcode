a1edd882cadf0f1529da093e1cff5b5be0b9ecee
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-10-07 05:12:26 +0200

    Update 207. Course Schedule.py

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -1,28 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def dfs_helper(i, adj_list, in_path, visited):
-            if in_path[i]:
-                return True
-            if visited[i]:
-                return False
-            in_path[i] = True
-            visited[i] = True
-            for nei in adj_list[i]:
-                if dfs_helper(nei, adj_list, in_path, visited):
-                    return False
-            in_path[i] = False
-            return False
-
         adj_list = [[] for _ in range(numCourses)]
-        for u, v in prerequisites:
-            if u == v:
-                return False
-            adj_list[v].append(u)
-        visited = [False]*(numCourses)
-        in_path = [False]*(numCourses)
-
-        for i in range(numCourses):
-            if dfs_helper(i, adj_list, in_path, visited):
-                return False
-        return True
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
         
+        visited = [0] * numCourses  # 0: NOT_VISITED, -1: VISITING, 1: VISITED
+        
+        def dfs(node):
+            if visited[node] == -1:  # Cycle detected (currently in the same path)
+                return False
+            if visited[node] == 1:   # Node has already been processed
+                return True
+            
+            visited[node] = -1  # Mark node as "VISITING"
+            
+            for neighbor in adj_list[node]:
+                if not dfs(neighbor):  # If any neighbor leads to a cycle
+                    return False
+            
+            visited[node] = 1  # Mark node as fully processed ("VISITED")
+            return True
+        
+        # Start DFS from each course (in case of disconnected components)
+        for node in range(numCourses):
+            if visited[node] == 0:  # Start DFS only for unvisited nodes
+                if not dfs(node):   # If a cycle is detected, return False
+                    return False
+        
+        return True  # If no cycle is detected, all courses can be finished
