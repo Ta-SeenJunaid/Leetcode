4847d8ade16fcc1434d7105e38f5e4814c667c8a
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-10-07 06:39:33 +0200

    Update 802. Find Eventual Safe States.py

--- 802. Find Eventual Safe States.py
+++ 802. Find Eventual Safe States.py
@@ -1,32 +1,19 @@
-# https://www.youtube.com/watch?v=Re_v0j0CRsg
-from typing import List
-
-
 class Solution:
-    def __init__(self):
-        self.safe = {}
-
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        res = []
-        for i in range(len(graph)):
-            if self.dfs(i, graph):
-                res.append(i)
-        return res
-
-    def dfs(self, i, graph: List[List[int]]):
-        if i in self.safe:
-            return self.safe[i]
-        self.safe[i] = False
-        for nei in graph[i]:
-            if not self.dfs(nei, graph):
+        visited = [0]*len(graph) # 0: NOT_VISITED, -1: VISITING, 1: VISITED
+        topo_res = []
+        def dfs(node):
+            if visited[node] == -1:
                 return False
-        self.safe[i] = True
-        return True
-
-
-solution = Solution()
-print(solution.eventualSafeNodes(graph = [[1,2],[2,3],[5],[0],[5],[],[]]))
-
-solution = Solution()
-print(solution.eventualSafeNodes(graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]))
-
+            if visited[node] == 1:
+                return True
+            visited[node] = -1
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            visited[node] = 1
+            return True
+        for node in range(len(graph)):
+            if dfs(node):
+                topo_res.append(node)
+        return topo_res
