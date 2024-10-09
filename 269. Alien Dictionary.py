a20c35c705f2291ae251d286a73facf8d2d0051c
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-10-09 17:36:10 +0200

    Update 269. Alien Dictionary.py

--- 269. Alien Dictionary.py
+++ 269. Alien Dictionary.py
@@ -1,56 +1,31 @@
 # https://www.youtube.com/watch?v=6kTZYvNNyps
-# https://www.youtube.com/watch?v=U3N_je7tWAs&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=26
-from typing import List
-
-
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         adj = {c:set() for w in words for c in w}
-        for i in range(len(words) - 1):
-          w1, w2 = words[i], words[i+1]
-          min_len = min(len(w1), len(w2))
-          if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
-            return ""
-          for j in range(min_len):
-            if w1[j] != w2[j]:
-              adj[w1[j]].add(w2[j])
-        # False = visited,  True = current path
-        visit = {}
-        res = []
-        def dfs(c):
-          if c in visit:
-            return visit[c]
-          visit[c] = True
-          for nei in adj[c]:
-            if dfs(nei):
-              return True
-          visit[c] = False
-          res.append(c)
-        for c in adj:
-          if dfs(c):
-            return ""
-        res.reverse()
-        return "".join(res)
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]: # w1 = "fantastic" and # w2 = "fan"
+                return ""
+            for j in range(min_len):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
 
-
-solution = Solution()
-print(solution.alienOrder(words=[
-  "wrt",
-  "wrf",
-  "er",
-  "ett",
-  "rftt"
-]))
-
-solution = Solution()
-print(solution.alienOrder(words=[
-  "z",
-  "x"
-]))
-
-solution = Solution()
-print(solution.alienOrder(words=[
-  "z",
-  "x",
-  "z"
-]))
+        visited = {} # True=visited, False=Current path & cycle
+        topo_ans = []
+        def dfs(node):
+            if node in visited:
+                return visited[node]
+            visited[node] = False
+            for nei in adj[node]:
+                if not dfs(nei):
+                    return False
+            visited[node] = True
+            topo_ans.append(node)
+            return True
+        for node in adj:
+            if not dfs(node):
+                return ""
+        return "".join(topo_ans[::-1])
+        
