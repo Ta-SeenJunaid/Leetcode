7beb4fac01ebb1fed7f880ac8f36bc04001e5480
Ta-Seen Junaid <taseen.junaid@gmail.com>
2024-10-21 16:46:11 +0200

    Create 2696. Minimum String Length After Removing Substrings.py

---
+++ 2696. Minimum String Length After Removing Substrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        for c in s:
+            if not stack:
+                stack.append(c)
+            elif c == "B" and stack[-1] == "A":
+                stack.pop()
+            elif c == "D" and stack[-1] == "C":
+                stack.pop()
+            else:
+                stack.append(c)
+        return len(stack)
+
+        
